aeace4e6ddc617279a74525232ee019f9997363c
vcimen <60547000+vcimen@users.noreply.github.com>
2020-11-26 19:36:15 -0500

    Add files via upload

--- directory_web_application/directory-app.py
+++ directory_web_application/directory-app.py
@@ -129,7 +129,7 @@ def before_request():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    if request.method == 'POST':
+    if request.method == 'GET':
         session.pop('user_id', None)
 
         username = request.form['username']
