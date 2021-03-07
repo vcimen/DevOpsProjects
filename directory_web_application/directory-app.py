2fb32ae9422523a22c90b922858d6dfd821aad76
vcimen <60547000+vcimen@users.noreply.github.com>
2021-03-07 10:30:27 -0500

    Add files via upload

--- directory_web_application/directory-app.py
+++ directory_web_application/directory-app.py
@@ -7,7 +7,7 @@ db_endpoint = open("/home/ec2-user/dbserver.endpoint", 'r', encoding='UTF-8')
 # Configure mysql database
 app.config['MYSQL_DATABASE_HOST'] = db_endpoint.readline().strip()
 app.config['MYSQL_DATABASE_USER'] = 'admin'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'Comp5331'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'admin'
 app.config['MYSQL_DATABASE_DB'] = 'directory'
 app.config['MYSQL_DATABASE_PORT'] = 3306
 db_endpoint.close()
